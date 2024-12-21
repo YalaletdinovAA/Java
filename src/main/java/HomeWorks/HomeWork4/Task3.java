75a47f8a8a30f2c1e119d7a0d7c280f2d8cbb960
YalaletdinovAA <yalaletdinov_azat@mail.ru>
2024-12-21 14:33:12 +0300

    1-4 домашки

---
+++ src/main/java/HomeWorks/HomeWork4/Task3.java
@@ -0,0 +1,21 @@
+package src.main.java.HomeWorks.HomeWork4;
+import java.util.Arrays;
+/*
+Задача3*.Задана строка,состоящая из букв английского алфавита,
+разделенных одним пробелом.
+Необходимо каждую последовательность символов упорядочить по возрастанию
+и вывести слова в нижнем регистре.
+Входные данные:в единственной строке последовательность символов представляющее два слова.
+Выходные данные: упорядоченные по возрастанию буквы в нижнем регистре.
+ */
+public class Task3 {
+    public static void main(String[] args) {
+
+        String input = "karabas barabas";
+        char[] charArray = input.toCharArray();
+        Arrays.sort(charArray);
+        String sortedString = new String(charArray);
+        System.out.println(sortedString);
+
+    }
+}
---
+++ src/main/java/HomeWorks/Homework2/Task3.java
@@ -0,0 +1,28 @@
+package src.main.java.HomeWorks.Homework2;
+/*
+Задача 3*.
+Напишите Java-программу для объединения данной строки ссамим собой заданное количество раз.
+Тестовые данные:Исходная строка: JavaСколько раз вывести строку? 8
+Ожидаемый результат :
+После повторения 8 раз: JavaJavaJavaJavaJavaJavaJavaJava
+ */
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Исходная строка:");
+        String inputString = scanner.nextLine();
+        System.out.println("Сколько раз вывести строку?");
+        int repeatCount = scanner.nextInt();
+
+        String result = "";
+        for (int i = 0; i < repeatCount; i++) {
+            result += inputString;
+        }
+        System.out.println("После повторения " + repeatCount + " раз: " + result);
+    }
+}
+
