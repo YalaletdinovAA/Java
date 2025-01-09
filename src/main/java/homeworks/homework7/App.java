ef67528d58a5e37e26f8235c9a74a2058def0fe8
YalaletdinovAA <yalaletdinov_azat@mail.ru>
2025-01-09 21:22:51 +0300

    HW5

--- src/main/java/homeworks/homework7/App.java
+++ src/main/java/homeworks/homework7/App.java
@@ -8,30 +8,13 @@ import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
+
         Product product = new Product(100,"Молоко");
         DiscountProduct discountProduct= new DiscountProduct(250,"Колбаса",10, LocalDate.of(2025,01,20),true);
 
         System.out.println(product.toString());
         System.out.println(discountProduct.toString());
 
-        try {
-            String string;
-            FileReader fileReader = new FileReader("src/main/java/homeworks/homework8/input.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            FileWriter fileWriter = new FileWriter("src/main/java/homeworks/homework8/output.txt");
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            while (bufferedReader.ready()) {
-                string = bufferedReader.readLine();
-                bufferedWriter.write(string + "\r\n");
-            }
-            bufferedReader.close();
-            fileReader.close();
-            bufferedWriter.flush();
-            bufferedWriter.close();
-
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
 
 
     }
